fc3a1aabc161017b0f1fd9ebfe4bb435b3579026
Chetra HONG <chetra.hong@axivit.com>
2023-11-18 23:25:49 +0700

    update socket url

--- src/common/websocket/index.js
+++ src/common/websocket/index.js
@@ -1,6 +1,7 @@
 import io from "socket.io-client";
-const BASE_URL = process.env.VUE_APP_API_URL;
-const socket = io(BASE_URL);
+// const BASE_URL = process.env.SOCKER_URL;
+// console.log(BASE_URL);
+const socket = io("https://my-app-aqw2.onrender.com");
 socket.on("connect", () => {
   console.log("Connected to the Socket.IO server");
 });
